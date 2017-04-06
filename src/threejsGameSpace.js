db5cdc34bc5ff2d06fe4ff91e25ec231c2407bb6
Mark Arndt <marndt_98@yahoo.com>
2017-04-06 08:13:16 -0700

    Make camera look at origin.

--- src/threejsGameSpace.js
+++ src/threejsGameSpace.js
@@ -89,6 +89,7 @@ GameSpace.prototype.go = function() {
 
 		requestAnimationFrame(update)
 		_appcontext.renderer.render(_appcontext.scene, _appcontext.myCamera)
+		_appcontext.myCamera.lookAt(new THREE.Vector(0,0,0));
 
 	}
 
